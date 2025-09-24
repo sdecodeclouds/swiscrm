04ce7b9bdebd978dfb4eed0a5edac5588a1d506e
Subhankar De <subhankar.de@codeclouds.com>
2025-09-24 13:48:12 +0530

    Remove all files from main

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    echo "<pre>";
-    print_r($_SESSION);
-    echo "</pre>";
-    //echo $_SESSION["prospect"]['data']['attributes']['bill_first_name']." ".$_SESSION["prospect"]['data']['attributes']['bill_last_name'];
-?>
